903f157ce52c2f470d83a9d9101b26d5d8e2f813
chenyp6 <517477566@qq.com>
2017-04-10 13:53:03 +0800

    2017.04.10    13.52    Addcart  success

--- src/main/java/com/cyp/gp/single/service/impl/SingleServiceImpl.java
+++ src/main/java/com/cyp/gp/single/service/impl/SingleServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cyp.gp.single.service.impl;
 
 import com.cyp.gp.cart.dao.Cart;
+import com.cyp.gp.cart.dao.CartKey;
 import com.cyp.gp.cart.mapper.CartMapper;
 import com.cyp.gp.hall.dao.Shoes;
 import com.cyp.gp.hall.mapper.ShoesMapper;
@@ -24,7 +25,19 @@ public class SingleServiceImpl implements SingleService {
     }
 
     public int AddToCart(Cart cart){
-       int i= cartMapper.insert(cart);
+        int i=0;
+        CartKey cartKey=new CartKey();
+        cartKey.setUserid(cart.getUserid());
+        cartKey.setShoesid(cart.getShoesid());
+        if (cartMapper.selectByPrimaryKey(cartKey)==null){
+            cartMapper.insert(cart);
+            i=1;
+        }
+        else{
+            cart.setNumber(cartMapper.selectByPrimaryKey(cartKey).getNumber()+cart.getNumber());
+            cartMapper.updateByPrimaryKey(cart);
+            i=1;
+        }
         return i;
     }
 }
